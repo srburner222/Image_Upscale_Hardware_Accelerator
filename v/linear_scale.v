885db6a0f4d9c874c4cd02e714937d24633b7aa0
Shawn Burner <srburner@linux-lab-060.ece.uw.edu>
2024-05-27 13:38:02 -0700

    Updated verilog files (pipelined); Updated testharness; Incorporated bsg_chip framework

--- v/linear_scale.v
+++
@@ -1,9 +0,0 @@
-module linear_scale #(parameter bit_depth = 8)
-(
- input  logic [bit_depth-1:0] a0, a1,
- output logic [bit_depth-1:0] data_out
-);
-   
-   assign data_out = (a0 >> 1) + (a1 >> 1);
-
-endmodule
